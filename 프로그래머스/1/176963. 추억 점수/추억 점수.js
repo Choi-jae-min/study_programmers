e4404e805a2a056123948e403ae7dfe3a6e988a7
jasmin <129919643+Choi-jae-min@users.noreply.github.com>
2025-11-30 19:59:15 +0900

    [level 1] Title: 추억 점수, Time: 6.33 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -15,3 +15,10 @@ function solution(name, yearning, photo) {
     }
     return answer;
 }
+// return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
+// photo 를 맵으로 배열 가져오고
+// 해당 배열을 reduce 함수로 축적
+// name.indexOf(c) 여기서 c가 may, kein등 의 데이터 name의 인덱스 번호 가져오고
+// 그 인덱스로 yearning 데이터 찾아서 축적 
+// 없으면 논리 연산자 ?? 로 0 추가
+// 
