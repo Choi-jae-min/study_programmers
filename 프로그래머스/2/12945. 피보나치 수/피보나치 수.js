026c91ca383e9efc0b2d9ed9df79c804c0375881
jasmin <129919643+Choi-jae-min@users.noreply.github.com>
2025-12-02 15:09:40 +0900

    [level 2] Title: 피보나치 수, Time: 4.78 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    var answer = 1;
+    var arr = [0,1]
+    for(i=2; i<=n; i++){
+        arr.push((arr[arr.length -1] + arr[arr.length -2]) % 1234567);
+    }
+    return arr[arr.length -1];
+}
