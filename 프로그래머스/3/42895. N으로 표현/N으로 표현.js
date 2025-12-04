d8dbeb82dcdae7390dcefc9c97b5024a7217b3f6
jasmin <129919643+Choi-jae-min@users.noreply.github.com>
2025-12-05 02:15:23 +0900

    [level 3] Title: N으로 표현, Time: 57.64 ms, Memory: 45.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -0,0 +1,27 @@
+function solution(N, number) {
+  if (N === number) return 1;
+  var dp = Array.from({ length: 9 }, () => new Set());
+  var concat = 0;
+
+  for (let i = 1; i <= 8; i++) {
+    concat = concat * 10 + N;
+    dp[i].add(concat);
+
+    for (let j = 1; j < i; j++) {
+      for (const a of dp[j]) {
+        for (const b of dp[i - j]) {
+          dp[i].add(a + b);
+          dp[i].add(a - b);
+          dp[i].add(b - a);
+          dp[i].add(a * b);
+          if (b !== 0) dp[i].add(a / b);
+          if (a !== 0) dp[i].add(b / a);
+        }
+      }
+    }
+
+    if (dp[i].has(number)) return i;
+  }
+
+  return -1;
+}
