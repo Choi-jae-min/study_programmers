dc29323fb4159dc04a3d0d06e91430759f464e92
jasmin <a01095567997@gmail.com>
2026-01-27 20:54:35 +0900

    [level 0] Title: 모음 제거, Time: 0.18 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,13 +1,17 @@
 class Solution {
     public String solution(String my_string) {
+        // String answer = "";
+        // for (String s : my_string.split("")){
+        //     if(s.equals("e") || s.equals("a") || s.equals("i")
+        //       || s.equals("o") || s.equals("u")){
+        //         continue;
+        //     }
+        //     answer += s;
+        // }
         String answer = "";
-        for (String s : my_string.split("")){
-            if(s.equals("e") || s.equals("a") || s.equals("i")
-              || s.equals("o") || s.equals("u")){
-                continue;
-            }
-            answer += s;
-        }
+
+        answer = my_string.replaceAll("[aeiou]", "");
+
         return answer;
     }
 }
