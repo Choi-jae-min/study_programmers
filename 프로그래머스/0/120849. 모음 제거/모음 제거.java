ef2c2ddea653ea4db8e99fa1a0db781dc9e2ea1a
jasmin <a01095567997@gmail.com>
2026-01-27 20:53:26 +0900

    [level 0] Title: 모음 제거, Time: 1.39 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for (String s : my_string.split("")){
+            if(s.equals("e") || s.equals("a") || s.equals("i")
+              || s.equals("o") || s.equals("u")){
+                continue;
+            }
+            answer += s;
+        }
+        return answer;
+    }
+}
