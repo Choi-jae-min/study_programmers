e721faf87a671fd314e8ae707625aaf1bdb19b8a
jasmin <a01095567997@gmail.com>
2026-01-28 12:52:16 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList();
+        
+        for(int i=1; i<=n; i++){
+            if(i % 2 ==1 ){answer.add(i);}
+        }
+        return answer;
+    }
+}
