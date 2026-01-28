1d84d1057a758c8eea1941c1316660ab74a91b9a
jasmin <a01095567997@gmail.com>
2026-01-28 12:46:29 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.18 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int[] newArr = array;
+        
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        return newArr[array.length/2];
+    }
+}
