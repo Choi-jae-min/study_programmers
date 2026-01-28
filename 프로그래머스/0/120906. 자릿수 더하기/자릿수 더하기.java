c46aedca560b0353261dea4cefd8de43e8f47d5b
jasmin <a01095567997@gmail.com>
2026-01-28 11:42:38 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        String[] list = str.split("");
+        for (String s : list){
+            answer +=  Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
