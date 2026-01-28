711124fbfcfd249d9f7f93bf9631d0f6ca36bad2
jasmin <a01095567997@gmail.com>
2026-01-28 12:30:57 +0900

    [level 0] Title: 수 조작하기 1, Time: 55.67 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        for (String s : control.split("")){
+            switch(s){
+                case "w" -> answer +=1;
+                case "s" -> answer -=1;
+                case "d" -> answer +=10;
+                case "a" -> answer -=10;
+            }
+        }
+        return answer;
+    }
+}
