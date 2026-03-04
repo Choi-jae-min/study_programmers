6296dbfdd38ba9345f58d3f6bcc2e04c0831fa6a
jasmin <a01095567997@gmail.com>
2026-03-04 09:45:16 +0900

    [level 1] Title: 시저 암호, Time: 1.24 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s, int n) {
+
+        StringBuilder answer = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+
+            if(c == ' '){
+                answer.append(' ');
+            }else if(Character.isUpperCase(c)){
+                answer.append((char)((c - 'A' + n) % 26 + 'A'));
+            }else{
+                answer.append((char)((c - 'a' + n) % 26 + 'a'));
+            }
+        }
+
+        return answer.toString();
+    }
+}
